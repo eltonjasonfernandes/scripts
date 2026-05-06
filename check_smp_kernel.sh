c1cb06e266a5a9fceb8e9eb2058793e8b9f8bbd6
EJF <93349578+eltonjasonfernandes@users.noreply.github.com>
2026-05-06 16:01:18 +0100

    Updated check_smp_kernel.sh
    Improved SMP kernel check script reliability and added SSH timeout

--- check_smp_kernel.sh
+++ check_smp_kernel.sh
@@ -1,33 +1,47 @@
-#!/bin/sh 
+#!/bin/sh
 ######################
 ## CHECK SMP KERNEL ##
 ######################
-MODULE=xt_RTPENGINE
-SIG_A= 127.0.0.1
-SIG_B= 10.140.3.136
-NULL= ''
+
+MODULE="xt_RTPENGINE"
+SIG_A="127.0.0.1"
+SIG_B="10.140.3.136"
+
 ######################
-if 
-lsmod | grep -qw "$MODULE" && ssh -q $SIG_B << EOF 
-lsmod | grep -qw "$MODULE"
-EOF
-then
-echo "CRITICAL - SMP Kernel Loaded On Both SIGs!"
-exit 2
+check_module() {
+    HOST=$1
 
-elif
-lsmod | grep -qw "$MODULE" || ssh -q $SIG_B << EOF 
-lsmod | grep -qw "$MODULE" 
-EOF
-then
-echo "OK - SMP Kernel is loaded!"
-exit 0
+    if [ "$HOST" = "127.0.0.1" ]; then
+        lsmod | grep -qw "$MODULE"
+        return $?
+    else
+        ssh -o ConnectTimeout=5 -q "$HOST" "lsmod | grep -qw $MODULE"
+        return $?
+    fi
+}
 
-elif
-lsmod | grep -qw "$MODULE" ; echo $? -eq 1 > /dev/null & ssh -q $SIG_B << EOF 
-lsmod | grep -qw "$MODULE" ; echo $? -eq 1 > /dev/null 
-EOF
-then
-echo "WARNING - SMP Kernel Unloaded on Both SIGs!"
-exit 1
+######################
+check_module "$SIG_A"
+A_STATUS=$?
+
+check_module "$SIG_B"
+B_STATUS=$?
+
+######################
+if [ $A_STATUS -eq 0 ] && [ $B_STATUS -eq 0 ]; then
+    echo "CRITICAL - SMP Kernel Loaded On Both SIGs!"
+    exit 2
+
+elif [ $A_STATUS -eq 0 ] || [ $B_STATUS -eq 0 ]; then
+    echo "OK - SMP Kernel is loaded on at least one SIG!"
+    exit 0
+
+elif [ $A_STATUS -ne 0 ] && [ $B_STATUS -ne 0 ]; then
+    echo "WARNING - SMP Kernel Unloaded on Both SIGs!"
+    exit 1
+
+else
+    echo "UNKNOWN - Unable to determine state"
+    exit 3
 fi
+
